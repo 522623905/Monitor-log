b82b323590ad0eb720dc385b7ebae97460ffc3ef
long <522623905@qq.com>
2018-08-15 11:37:00 +0800

    整理

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# imooc_logprocess
+慕课网视频的一个日志监控实例整理
 
-Source code for imooc [https://www.imooc.com/learn/982](https://www.imooc.com/learn/982)
+log_process主程序
+mock_data模拟往access.log写输入
+
+安装fluxdb并运行:
+	sudo docker pull tutum/influxdb
+	sudo docker run -d -p 8083:8083 -p8086:8086 --expose 8090 --expose 8099 --name influxsrv tutum/influxdb
+创建数据库:
+	...
+浏览器输入:
+	http://127.0.0.1:8083/
+	创建数据库:CREATE DATABASE "imooc"
+
+安装grafana:
+	docker run -d --name=grafana -p 3000:3000 grafana/grafana
+浏览器登录localhost:3000:
+	配置DATA Sources为influxdb,才能读取到influxdb的数据
+
+运行:
+	./mock_data
+	./log_process -path ./access.log -influxDsn http://127.0.0.1:8086@imooc@imoocpass@imooc@s
+
+浏览器打开:
+	localhost:3000 可查看监控的图表
+	
+http可查看程序的部分监控信息:
+	curl 127.0.0.1:9193/monitor 
--- log_process.go
+++ log_process.go
@@ -18,19 +18,21 @@ import (
 	"github.com/influxdata/influxdb/client/v2"
 )
 
+//读取接口
 type Reader interface {
 	Read(rc chan []byte)
 }
 
+//写入接口
 type Writer interface {
 	Write(wc chan *Message)
 }
 
 type LogProcess struct {
-	rc    chan []byte
-	wc    chan *Message
-	read  Reader
-	write Writer
+	rc    chan []byte   //负责读取模块到解析模块的数据传输
+	wc    chan *Message //负责解析模块到写入模块的数据传输
+	read  Reader        //读取模块
+	write Writer        //写入模块
 }
 
 type ReadFromFile struct {
@@ -41,6 +43,7 @@ type WriteToInfluxDB struct {
 	influxDBDsn string // influx data source
 }
 
+//存储提取出来的监控数据
 type Message struct {
 	TimeLocal                    time.Time
 	BytesSent                    int
@@ -63,17 +66,21 @@ const (
 	TypeErrNum     = 1
 )
 
+//用于统计行数和错误数的数据传输channel
 var TypeMonitorChan = make(chan int, 200)
 
+//监控模块的封装
 type Monitor struct {
 	startTime time.Time
 	data      SystemInfo
 	tpsSli    []int
 }
 
+//开始监控
 func (m *Monitor) start(lp *LogProcess) {
 
 	go func() {
+		//统计行数和错误数
 		for n := range TypeMonitorChan {
 			switch n {
 			case TypeErrNum:
@@ -84,11 +91,13 @@ func (m *Monitor) start(lp *LogProcess) {
 		}
 	}()
 
+	//每5s统计一次tps
 	ticker := time.NewTicker(time.Second * 5)
 	go func() {
 		for {
 			<-ticker.C
 			m.tpsSli = append(m.tpsSli, m.data.HandleLine)
+			//保持最新的两个总的日志行数就可以了
 			if len(m.tpsSli) > 2 {
 				m.tpsSli = m.tpsSli[1:]
 			}
@@ -96,23 +105,25 @@ func (m *Monitor) start(lp *LogProcess) {
 	}()
 
 	http.HandleFunc("/monitor", func(writer http.ResponseWriter, request *http.Request) {
-		m.data.RunTime = time.Now().Sub(m.startTime).String()
+		m.data.RunTime = time.Now().Sub(m.startTime).String() //计算当前程序的运行时间
 		m.data.ReadChanLen = len(lp.rc)
 		m.data.WriteChanLen = len(lp.wc)
 
+		//每5s统计一次tps, tps=(总日志数2-总日志数1)/5
 		if len(m.tpsSli) >= 2 {
 			m.data.Tps = float64(m.tpsSli[1]-m.tpsSli[0]) / 5
 		}
 
-		ret, _ := json.MarshalIndent(m.data, "", "\t")
+		//渲染
+		ret, _ := json.MarshalIndent(m.data, "", "\t") //转出来的json打印出来比较好看
 		io.WriteString(writer, string(ret))
 	})
 
 	http.ListenAndServe(":9193", nil)
 }
 
+// 读取模块:读取日志文件后交给解析模块
 func (r *ReadFromFile) Read(rc chan []byte) {
-	// 读取模块
 
 	// 打开文件
 	f, err := os.Open(r.path)
@@ -121,28 +132,31 @@ func (r *ReadFromFile) Read(rc chan []byte) {
 	}
 
 	// 从文件末尾开始逐行读取文件内容
-	f.Seek(0, 2)
-	rd := bufio.NewReader(f)
+	// 因为既然是监控,我们想要读取的是最新的数据,所有选择从末尾读取
+	f.Seek(0, 2)             //把文件指针移动到文件末尾
+	rd := bufio.NewReader(f) //因为f只能一个一个字节的读,因此先用bufio转成一个新的rd,支持更多读取操作
 
+	//循环逐行读取文件
 	for {
-		line, err := rd.ReadBytes('\n')
+		line, err := rd.ReadBytes('\n') //读取内容直到遇到\n,即读取一行内容
+		//EOF表示读取到文件末尾,则sleep,等有新的信息产生
 		if err == io.EOF {
 			time.Sleep(500 * time.Millisecond)
 			continue
 		} else if err != nil {
 			panic(fmt.Sprintf("ReadBytes error:%s", err.Error()))
 		}
-		TypeMonitorChan <- TypeHandleLine
-		rc <- line[:len(line)-1]
+		TypeMonitorChan <- TypeHandleLine //读取到一行,则添加一个标记,用于后续统计读取个数
+		rc <- line[:len(line)-1]          //这里是表示不要最后的换行符
 	}
 }
 
+// 写入模块:把从解析模块得到的数据交给influxdb存储
 func (w *WriteToInfluxDB) Write(wc chan *Message) {
-	// 写入模块
 
 	infSli := strings.Split(w.influxDBDsn, "@")
 
-	// Create a new HTTPClient
+	// Create a new influxdb HTTPClient
 	c, err := client.NewHTTPClient(client.HTTPConfig{
 		Addr:     infSli[0],
 		Username: infSli[1],
@@ -156,7 +170,7 @@ func (w *WriteToInfluxDB) Write(wc chan *Message) {
 		// Create a new point batch
 		bp, err := client.NewBatchPoints(client.BatchPointsConfig{
 			Database:  infSli[3],
-			Precision: infSli[4],
+			Precision: infSli[4], //精度,秒s
 		})
 		if err != nil {
 			log.Fatal(err)
@@ -187,8 +201,8 @@ func (w *WriteToInfluxDB) Write(wc chan *Message) {
 	}
 }
 
+// 解析模块:正则解析读取模块传递的数据,并交给写入模块
 func (l *LogProcess) Process() {
-	// 解析模块
 
 	/**
 	172.0.0.12 - - [04/Mar/2018:13:49:52 +0000] http "GET /foo?query=t HTTP/1.0" 200 2133 "-" "KeepAliveClient" "-" 1.005 1.854
@@ -196,19 +210,21 @@ func (l *LogProcess) Process() {
 
 	r := regexp.MustCompile(`([\d\.]+)\s+([^ \[]+)\s+([^ \[]+)\s+\[([^\]]+)\]\s+([a-z]+)\s+\"([^"]+)\"\s+(\d{3})\s+(\d+)\s+\"([^"]+)\"\s+\"(.*?)\"\s+\"([\d\.-]+)\"\s+([\d\.-]+)\s+([\d\.-]+)`)
 
-	loc, _ := time.LoadLocation("Asia/Shanghai")
+	loc, _ := time.LoadLocation("Asia/Shanghai") //上海时区
 	for v := range l.rc {
 		ret := r.FindStringSubmatch(string(v))
 		if len(ret) != 14 {
-			TypeMonitorChan <- TypeErrNum
+			TypeMonitorChan <- TypeErrNum //读取到错误,则添加标记
 			log.Println("FindStringSubmatch fail:", string(v))
 			continue
 		}
 
 		message := &Message{}
+
+		//解析出时间
 		t, err := time.ParseInLocation("02/Jan/2006:15:04:05 +0000", ret[4], loc)
 		if err != nil {
-			TypeMonitorChan <- TypeErrNum
+			TypeMonitorChan <- TypeErrNum //读取到错误,则添加标记
 			log.Println("ParseInLocation fail:", err.Error(), ret[4])
 			continue
 		}
@@ -218,9 +234,9 @@ func (l *LogProcess) Process() {
 		message.BytesSent = byteSent
 
 		// GET /foo?query=t HTTP/1.0
-		reqSli := strings.Split(ret[6], " ")
+		reqSli := strings.Split(ret[6], " ") //按空格分割
 		if len(reqSli) != 3 {
-			TypeMonitorChan <- TypeErrNum
+			TypeMonitorChan <- TypeErrNum //读取到错误,则添加标记
 			log.Println("strings.Split fail", ret[6])
 			continue
 		}
@@ -229,7 +245,7 @@ func (l *LogProcess) Process() {
 		u, err := url.Parse(reqSli[1])
 		if err != nil {
 			log.Println("url parse fail:", err)
-			TypeMonitorChan <- TypeErrNum
+			TypeMonitorChan <- TypeErrNum //读取到错误,则添加标记
 			continue
 		}
 		message.Path = u.Path
@@ -248,6 +264,7 @@ func (l *LogProcess) Process() {
 
 func main() {
 	var path, influxDsn string
+	//用于解析参数
 	flag.StringVar(&path, "path", "./access.log", "read file path")
 	flag.StringVar(&influxDsn, "influxDsn", "http://127.0.0.1:8086@imooc@imoocpass@imooc@s", "influx data source")
 	flag.Parse()
